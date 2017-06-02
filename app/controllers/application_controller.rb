74f31bf9e7ede5904e01b99c68c3ca6a764b68fe
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-02 20:28:23 +0000

    Basic site navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   require 'pry'
+
+  helper_method :logged_in?, :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ get '/signup' => "registrations#new", as: :new_user
+ post '/users/create', to: "registrations#create", as: :create_new_user
+
+ get '/signin' => "sessions#new", as: :new_session
+ post '/sessions/create', to: "sessions#create", as: :create_new_session
+ delete '/signout' => "sessions#destroy", as: :destroy_session
+
+ resources :users, only: [:show, :destroy, :edit, :update]
+
+
+ # get 'users/:id' => "users#show" as: :show_user
+ # delete 'users/:id' => "users#destroy" as: :destroy_user
+ # get 'users/:id/edit' => "users#edit" as: :user_edit
+ # patch 'users/:id' => "users#update" as:
+
+ root 'welcome#greeting'
 end
