7e1ac93dafc23ffe3f755f14c1a85560b81f4bac
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-06 01:22:06 +0000

    Omniauth not working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, "1025298811953-gnnbqubvj0advfbdiuohqckhq0hjg2o6.apps.googleusercontent.com", "-1TwWv5regYB_6bXG6DhmdD_"
 end
