2dcee0d7cad37f70ae6accc85b14db896d785a35
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-10 04:38:08 -0400

    Added liks in habits show page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,11 +1,10 @@
 class GoalsController < ApplicationController
-  # before_action :set_goal, :set_habit, only: [:show, :edit, :update, :destroy]
-  before_action :set_goal, :set_habit, only: [:show, :update_milestones, :update]
+  before_action :set_goal, :set_habit, only: [:show, :update_milestones, :update, :edit, :destroy]
 
 
-  # def index
-  #   @goals = @habit.goals
-  # end
+  def index
+    @goals = @habit.goals
+  end
 
   # def new
   # end
@@ -26,18 +25,11 @@ class GoalsController < ApplicationController
     end
   end
 
-  # <ActionController::Parameters {"utf8"=>"✓", "_method"=>"put",
-  #   "authenticity_token"=>"jiTAuK8jGfEr2K+zz7lskLweDXg76bGoBGdtqYb+g4EzABQYKop5+eqTpxRfwiY/iTwbGJnW5vRPqCeXnewB7w==",
-  #   "/habits/1/goals/1/milestones/1"=>{"milestones"=>{"description"=>""}, "milestone_ids"=>"45"}, "commit"=>"Update Goal",
-  #   "controller"=>"goals", "action"=>"update", "habit_id"=>"1", "id"=>"1"} permitted: false>
-
-
-
   def show
   end
 
-  # def destroy
-  # end
+  def destroy
+  end
 
   def update_milestones
     # binding.pry
@@ -67,9 +59,7 @@ class GoalsController < ApplicationController
     redirect_to habit_goal_path(@habit.id, @goal.id)
   end
 
-  # ActionController::Parameters {"utf8"=>"✓", "_method"=>"patch", "authenticity_token"=>"YcBXjEm6ClbwPfbsvEPF2Y/4ndP9PLJ0SsRTKeKFagzc5IMszBNqXjF2/kssOI92utqLs18D5SgBCxkX+ZfoYg==", "
-  #   goal"=>{"milestones_attributes"=>{"0"=>{"description"=>""}}, "milestone_ids"=>"11"},
-  # "commit"=>"Add Milestones", "controller"=>"goals", "action"=>"update_milestones", "habit_id"=>"10", "id"=>"10"} permitted: false>
+
   private
 
     def set_goal
@@ -83,10 +73,6 @@ class GoalsController < ApplicationController
     def set_user
       @user = @habit.user
     end
-
-    # def set_user
-    #   @user = @habit.user
-    # end
 
     def goal_params
       params.required(:goal).permit(:description, :habit_id, :milestone_ids => [], :milestones_attributes => [:description])
