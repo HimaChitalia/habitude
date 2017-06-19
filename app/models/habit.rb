48cdb0bf9ff434832ab9829b1e31cdcf90815472
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-19 16:05:50 -0400

    Trying to dry up function

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -14,20 +14,17 @@ class Habit < ApplicationRecord
     :allow_destroy => true
 
     def add_goal(goal)
-      # locations_will_change!
       self.goals = [] if self.goals == nil
       update_attributes goals: self.goals.push(goal)
     end
 
     def self.search(search)
-      if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
-        where("category_id::text LIKE ?", search)
-      elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
-        where("category_id LIKE ?", search)
-      end
-      # where("category_id LIKE ?", search.to_i)
-      # category = Category.find_by(name: search)
-        # where("category_id LIKE ?", category_id)
+      # if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
+      #   where("category_id::text LIKE ?", search)
+      # elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "sqlite3"
+      #   where("category_id LIKE ?", search)
+      # end
+      scope :search, ->(search) { where(category_id: search) }
     end
 
 end
