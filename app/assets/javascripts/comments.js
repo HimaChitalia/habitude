432a10d5069b48d12b0b38c233d8e7b2c7f86808
Hima Chitalia <hima_chhag@yahoo.com>
2017-07-26 12:49:21 -0400

    Delete comment via JQuery

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,9 +3,23 @@ var buildComment = function (object){
  var values = form.serialize();
  var myString = form.attr("action")
  var path = myString.replace('http://localhost:3000/habits/','');
+ var textEmpty =  $("textarea[id=comment_description]")
  var userName = $("input[id=comment_user_name]").val()
  $.post(path, values)
      .done(function(data) {
+       textEmpty.val('');
+       var children = $("div.allComments > div").length
+       var title = ""
+       console.log(children)
+       debugger;
+       if (children >= 2){
+         title = "<h5 class='commentTitle'> Comments: </h5><br/>"
+         $("div.commentHeader").html(title)
+       } else if (children === 1) {
+         title = "<h5 class='commentTitle'> Comment: </h5><br/>"
+         $("div.commentHeader").html(title)
+       }
+
          data.user_name = userName
          var comment = new Comment(data)
          $('div.allComments').append(comment.showComment())
@@ -33,10 +47,46 @@ function Comment(comment){
 
 Comment.prototype.showComment = function(){
   let commentHtml = `
+  <div class="commentDiv-${this.id}">
   <h5> » ${this.description}</h5>
   <h5> User: ${this.user_name}</h5>
   <a href="/habits/${this.habit_id}/comments/${this.id}/edit"  data-method="get">Edit</a>
-  <a href="/habits/${this.habit_id}/comments/${this.id}" data-confirm="Destroy Comment: '${this.description}'?" data-method="delete" rel="nofollow">Delete</a>
+  <a href="/habits/${this.habit_id}/comments/${this.id}" class="js-delete-comment" data-confirm="Destroy Comment: '${this.description}'?" >Delete</a>
+  </div>
   `
   return commentHtml
 }
+
+
+$(document).on("click", 'a.js-delete-comment', function(event){
+  event.preventDefault();
+  var comment = this
+  var children = $("div.allComments > div").length
+  var title = ""
+  console.log(children)
+  debugger;
+  if (children > 3){
+    title = "<h5 class='commentTitle'> Comments: </h5> <br/>"
+    $("div.commentHeader").html(title)
+  } else if (children === 3) {
+    title = "<h5 class='commentTitle'> Comment: </h5> <br/>"
+    $("div.commentHeader").html(title)
+  }else if (children === 2) {
+    title = "<h5 class='commentTitle'>There are no comments for this Habit yet! </h5>"
+    $("div.commentHeader").html(title)
+  }
+  // debugger;
+  comment.parentElement.remove()
+  $.ajax({
+        type: 'DELETE',
+        url: comment.href,
+        dataType: "json",
+        data: {"_method":"delete"},
+        complete: function(){
+            alert("Deleted Successfully");
+        },
+        error: function(result){
+          "something went wrong" + result
+        }
+    });
+})
